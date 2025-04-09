41cf13e8d9e5e47a92963099cc21c9a2a4f51943
Basant <Basant@DESKTOP-8TJMTA0>
2025-04-09 13:44:47 +0530

    pullingtestcase

---
+++ OrangeHrM22222222222222/src/test/java/runnerScripts/TC_O3_TestPulling.java
@@ -0,0 +1,15 @@
+package runnerScripts;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC_O3_TestPulling {
+	
+	@Test
+	
+	public void testcase()
+	{
+		System.out.println("checking pull request");
+	}
+
+}
