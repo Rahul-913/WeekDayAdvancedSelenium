a824e3aa6f3161d299ebd1e0c55c5884c4c0aaea
Dell <Dell@DESKTOP-G9LOSHP-K-R-ANVITH>
2025-04-09 14:01:28 +0530

    done

--- OrangeHrM22222222222222/src/test/java/runnerScripts/Just_Practice.java
+++ OrangeHrM22222222222222/src/test/java/runnerScripts/Just_Practice.java
@@ -3,8 +3,8 @@ package runnerScripts;
 import org.testng.annotations.Test;
 
 public class Just_Practice {
-@Test
-public void practice() {
-	System.out.println("This is for practice purpose");
-}
+	@Test
+	public void practice() {
+		System.out.println("This is for practice purpose");
+	}
 }
