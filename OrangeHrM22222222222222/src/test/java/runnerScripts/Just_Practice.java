27277f892e016605f266fd15413789b8127f3090
Dell <Dell@DESKTOP-G9LOSHP-K-R-ANVITH>
2025-04-09 13:39:39 +0530

    practice test case has been committed

---
+++ OrangeHrM22222222222222/src/test/java/runnerScripts/Just_Practice.java
@@ -0,0 +1,10 @@
+package runnerScripts;
+
+import org.testng.annotations.Test;
+
+public class Just_Practice {
+@Test
+public void practice() {
+	System.out.println("This is for practice purpose");
+}
+}
