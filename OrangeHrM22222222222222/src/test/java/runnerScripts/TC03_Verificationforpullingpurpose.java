4c1f7f2e0b17ac51917c321805162606f482ee80
kavya19980101 <kavyastestings@gmail.com>
2025-04-09 13:38:44 +0530

    Testcase3 added

---
+++ OrangeHrM22222222222222/src/test/java/runnerScripts/TC03_Verificationforpullingpurpose.java
@@ -0,0 +1,14 @@
+package runnerScripts;
+
+import org.testng.annotations.Test;
+
+public class TC03_Verificationforpullingpurpose {
+
+	@Test
+	public void testcase03()
+	{
+		System.out.println("Pull check");
+	}
+	
+	
+}
